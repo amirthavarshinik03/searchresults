1c8c24d28531ebfed46f4a6ef312c8a2bb50e3b1
amirthavarshinik03 <amirthavarshinik11@gmail.com>
2024-08-19 19:56:22 +0530

    Add files via upload

---
+++ data/initialDetails.js
@@ -0,0 +1,22 @@
+// src/data/initialDetails.js
+
+const initialDetails = [
+    {
+      id: 1,
+      imgPath: "/assets/img/shirt1.jpg",
+      name: "Black Shirt",
+    },
+    {
+      id: 2,
+      imgPath: "/assets/img/blackshirt.jpg",
+      name: "Side view black shirt",
+    },
+    {
+      id: 3,
+      imgPath: "/assets/img/shirt2.jpg",
+      name: "Back view black shirt",
+    },
+    
+  ];
+  
+  export default initialDetails;
